78ee4751cb0bc1976a06ad605ad4829549e1bce7
Rafael Pignataro <rafael.pignataro@singu.com.br>
2021-09-08 15:34:09 -0300

    Added logs to routes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+  console.info('Route: /');
   return res.json({ message: '/' });
 })
 
 app.get('/teste', (req, res) => {
+  console.info('Route: /teste');
   return res.json({ message: '/teste' });
 })
 
