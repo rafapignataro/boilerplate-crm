c878942456362e97f0c8e1703af1d00e30937672
Rafael Pignataro <rafael.pignataro@singu.com.br>
2021-09-08 15:31:34 -0300

    Add route to test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.json({ ok: true });
+  return res.json({ message: '/' });
+})
+
+app.get('/teste', (req, res) => {
+  return res.json({ message: '/teste' });
 })
 
 app.listen(3000);
